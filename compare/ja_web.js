85db2e1a90de2e1d53207bfaaedb7ec8d2029cdb
E24056750 <homelinux02@gmail.com>
2018-05-22 23:58:24 +0800

    aaaaaa

--- compare/ja_web.js
+++ compare/ja_web.js
@@ -12,13 +12,26 @@ button2.onclick=function(){
 		alert("a1="+a1.height+" "+a1.width+" a2=  "+ a2.height+" "+ a2.width);
 }
 button3.onclick=function(){
+		//p1資料處理
 		var c1= document.getElementById("canvas");
 		var ctx1=c1.getContext('2d');
 		var img1 = document.getElementById("a1")
 		ctx1.drawImage(img1,0,0);
 		var p1 = ctx1.getImageData(0,0,100,100);
-		//var p2 = img2.getImageData(0,0,100,100);
+		//p2資料處理
+		var c2= document.getElementById("canvas");
+		var ctx2=c2.getContext('2d');
+		var img2 = document.getElementById("a2")
+		ctx2.drawImage(img2,0,0);
+		var p2 = ctx2.getImageData(0,0,100,100);
+		//開始比對
+		var count =0;
+		for(var i=0;i<10000;i++){
+			if( p2.data[i]>p1.data[i]-10 && p2.data[i]<p1.data[i]+10){
+				count++;
+			}
+		}
 		
-		alert("p1="+p1.data[2]);
+		alert("相似度="+ count/10000);
 }
 
