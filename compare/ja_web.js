f919eff2eff1aba5f893f971a977afa3428ccf15
E24056750 <homelinux02@gmail.com>
2018-05-27 21:59:00 +0800

    qweee

--- compare/ja_web.js
+++ compare/ja_web.js
@@ -1,16 +1,14 @@
-
+var a=0
 
 button1.onclick=function(){
-		alert("a1="+a1.height+" "+a1.width+" a2=  "+ a2.height+" "+ a2.width);
+		a3.style.display="none";
+		a2.style.display="block";
+		a=1;
 }
 button2.onclick=function(){
-		a1.style.height=100+"px";
-		a1.style.width=100+"px";
-		a2.style.height=100+"px";
-		a2.style.width=100+"px";
-		a3.style.height=100+"px";
-		a3.style.width=100+"px";
-		alert("a1="+a1.height+" "+a1.width+" a2=  "+ a2.height+" "+ a2.width);
+		a2.style.display="none";
+		a3.style.display="block";
+		a=2;
 }
 button3.onclick=function(){
 		//p1資料處理
@@ -19,21 +17,14 @@ button3.onclick=function(){
 		var img1 = document.getElementById("a1")
 		ctx1.drawImage(img1,0,0);
 		var p1 = ctx1.getImageData(0,0,100,100);
+		if(a==1){
 		//p2資料處理
 		var c2= document.getElementById("canvas");
 		var ctx2=c2.getContext('2d');
 		var img2 = document.getElementById("a2")
 		ctx2.drawImage(img2,0,0);
 		var p2 = ctx2.getImageData(0,0,100,100);
-		//p3資料處理
-		var c3= document.getElementById("canvas");
-		var ctx3=c3.getContext('2d');
-		var img3 = document.getElementById("a3")
-		ctx3.drawImage(img3,0,0);
-		var p3 = ctx3.getImageData(0,0,100,100);
-		//開始比對
-		alert(p1.data.length);
-		var count =0;
+				var count =0;
 		for(var i=0;i<40000;){
 			if( p2.data[i]>p1.data[i]-10&& p2.data[i]<p1.data[i]+10){
 				count++;
@@ -46,7 +37,16 @@ button3.onclick=function(){
 			}
       i=i+4;
 		}
-		///測試
+		alert("x count ="+count+" 相似度= "+ count/400 +"%");
+		}
+		//p3資料處理
+		if(a==2){
+		var c3= document.getElementById("canvas");
+		var ctx3=c3.getContext('2d');
+		var img3 = document.getElementById("a3")
+		ctx3.drawImage(img3,0,0);
+		var p3 = ctx3.getImageData(0,0,100,100);
+			///測試
 		var count_test =0;
 				for(var i=0;i<40000;){
 				if( p3.data[i]>p1.data[i]-10 && p3.data[i]<p1.data[i]+10){
@@ -56,9 +56,16 @@ button3.onclick=function(){
 			}				if( p3.data[i+2]>p1.data[i+2]-10 && p3.data[i+2]<p1.data[i+2]+10){
 				count_test++;
 			}i=i+4;
-		}
-		///
-		alert("x count ="+count+" 相似度= "+ count/400 +"%");
+		}		
+		
 		alert(" count_test ="+count_test+" 相似度= "+ count_test/400 +"%");
+		}
+		//開始比對
+		
+
+	
+		///
+		
+		
 }
 
