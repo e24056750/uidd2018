2f333147c7e381a7b97d508777d53a7d4adc7baf
E24056750 <homelinux02@gmail.com>
2018-05-27 22:09:12 +0800

    rrr

--- compare/ja_web.js
+++ compare/ja_web.js
@@ -1,13 +1,13 @@
-var a=0
+var a=0;
 
 button1.onclick=function(){
-		a3.style.display="none";
-		a2.style.display="block";
+		a3.style.opacity=0;
+		a2.style.opacity=100;
 		a=1;
 }
 button2.onclick=function(){
-		a2.style.display="none";
-		a3.style.display="block";
+		a3.style.opacity=100;
+		a2.style.opacity=0;
 		a=2;
 }
 button3.onclick=function(){
@@ -17,14 +17,20 @@ button3.onclick=function(){
 		var img1 = document.getElementById("a1")
 		ctx1.drawImage(img1,0,0);
 		var p1 = ctx1.getImageData(0,0,100,100);
-		if(a==1){
 		//p2資料處理
 		var c2= document.getElementById("canvas");
 		var ctx2=c2.getContext('2d');
 		var img2 = document.getElementById("a2")
 		ctx2.drawImage(img2,0,0);
 		var p2 = ctx2.getImageData(0,0,100,100);
-				var count =0;
+		//p3資料處理
+		var c3= document.getElementById("canvas");
+		var ctx3=c3.getContext('2d');
+		var img3 = document.getElementById("a3")
+		ctx3.drawImage(img3,0,0);
+		var p3 = ctx3.getImageData(0,0,100,100);
+		//開始比對
+		var count =0;
 		for(var i=0;i<40000;){
 			if( p2.data[i]>p1.data[i]-10&& p2.data[i]<p1.data[i]+10){
 				count++;
@@ -37,16 +43,7 @@ button3.onclick=function(){
 			}
       i=i+4;
 		}
-		alert("x count ="+count+" 相似度= "+ count/400 +"%");
-		}
-		//p3資料處理
-		if(a==2){
-		var c3= document.getElementById("canvas");
-		var ctx3=c3.getContext('2d');
-		var img3 = document.getElementById("a3")
-		ctx3.drawImage(img3,0,0);
-		var p3 = ctx3.getImageData(0,0,100,100);
-			///測試
+		///測試
 		var count_test =0;
 				for(var i=0;i<40000;){
 				if( p3.data[i]>p1.data[i]-10 && p3.data[i]<p1.data[i]+10){
@@ -56,16 +53,11 @@ button3.onclick=function(){
 			}				if( p3.data[i+2]>p1.data[i+2]-10 && p3.data[i+2]<p1.data[i+2]+10){
 				count_test++;
 			}i=i+4;
-		}		
-		
-		alert(" count_test ="+count_test+" 相似度= "+ count_test/400 +"%");
 		}
-		//開始比對
-		
-
-	
 		///
-		
-		
+		if(a==1){
+		alert("count ="+count+" 相似度= "+ count/400 +"%");}
+		if(a==2){
+		alert(" count_test ="+count_test+" 相似度= "+ count_test/400 +"%");}
 }
 
