1ad4a6955fad3c8df48edfd4387af26c13789f09
E24056750 <homelinux02@gmail.com>
2018-05-27 13:11:25 +0800

    qqw

--- compare/ja_web.js
+++ compare/ja_web.js
@@ -4,12 +4,12 @@ button1.onclick=function(){
 		alert("a1="+a1.height+" "+a1.width+" a2=  "+ a2.height+" "+ a2.width);
 }
 button2.onclick=function(){
-		a1.style.height=300+"px";
-		a1.style.width=300+"px";
-		a2.style.height=300+"px";
-		a2.style.width=300+"px";
-		a3.style.height=300+"px";
-		a3.style.width=300+"px";
+		a1.style.height=100+"px";
+		a1.style.width=100+"px";
+		a2.style.height=100+"px";
+		a2.style.width=100+"px";
+		a3.style.height=100+"px";
+		a3.style.width=100+"px";
 		alert("a1="+a1.height+" "+a1.width+" a2=  "+ a2.height+" "+ a2.width);
 }
 button3.onclick=function(){
@@ -18,47 +18,47 @@ button3.onclick=function(){
 		var ctx1=c1.getContext('2d');
 		var img1 = document.getElementById("a1")
 		ctx1.drawImage(img1,0,0);
-		var p1 = ctx1.getImageData(0,0,300,300);
+		var p1 = ctx1.getImageData(0,0,100,100);
 		//p2資料處理
 		var c2= document.getElementById("canvas");
 		var ctx2=c2.getContext('2d');
 		var img2 = document.getElementById("a2")
 		ctx2.drawImage(img2,0,0);
-		var p2 = ctx2.getImageData(0,0,300,300);
+		var p2 = ctx2.getImageData(0,0,100,100);
 		//p3資料處理
 		var c3= document.getElementById("canvas");
 		var ctx3=c3.getContext('2d');
 		var img3 = document.getElementById("a3")
 		ctx3.drawImage(img3,0,0);
-		var p3 = ctx3.getImageData(0,0,300,300);
+		var p3 = ctx3.getImageData(0,0,100,100);
 		//開始比對
 		alert(p1.data.length);
 		var count =0;
-		for(var i=0;i<360000;){
-			if( p2.data[i]>p1.data[i]-3&& p2.data[i]<p1.data[i]+3){
+		for(var i=0;i<40000;){
+			if( p2.data[i]>p1.data[i]-10&& p2.data[i]<p1.data[i]+10){
 				count++;
 			}
-						if( p2.data[i+1]>p1.data[i+1]-3 && p2.data[i+1]<p1.data[i+1]+3){
+						if( p2.data[i+1]>p1.data[i+1]-10 && p2.data[i+1]<p1.data[i+1]+10){
 				count++;
 			}
-						if( p2.data[i+2]>p1.data[i+2]-3 && p2.data[i+2]<p1.data[i+2]+3){
+						if( p2.data[i+2]>p1.data[i+2]-10 && p2.data[i+2]<p1.data[i+2]+10){
 				count++;
 			}
       i=i+4;
 		}
 		///測試
 		var count_test =0;
-				for(var i=0;i<360000;){
-				if( p3.data[i]>p1.data[i]-3 && p3.data[i]<p1.data[i]+3){
+				for(var i=0;i<40000;){
+				if( p3.data[i]>p1.data[i]-10 && p3.data[i]<p1.data[i]+10){
 				count_test++;
-			}				if( p3.data[i+1]>p1.data[i+1]-3 && p3.data[i+1]<p1.data[i+1]+3){
+			}				if( p3.data[i+1]>p1.data[i+1]-10 && p3.data[i+1]<p1.data[i+1]+10){
 				count_test++;
-			}				if( p3.data[i+2]>p1.data[i+2]-3 && p3.data[i+2]<p1.data[i+2]+3){
+			}				if( p3.data[i+2]>p1.data[i+2]-10 && p3.data[i+2]<p1.data[i+2]+10){
 				count_test++;
 			}i=i+4;
 		}
 		///
-		alert("a count ="+count+" 相似度= "+ count/2700 +"%");
-		alert(" count_test ="+count_test+" 相似度= "+ count_test/2700 +"%");
+		alert("x count ="+count+" 相似度= "+ count/400 +"%");
+		alert(" count_test ="+count_test+" 相似度= "+ count_test/400 +"%");
 }
 
